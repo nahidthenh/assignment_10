728b35c815c462495d15342d644dffdaf8def3ad
nahidthenh <nhgroup18@gmail.com>
2021-10-18 09:31:24 +0600

    Footer

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,23 +1,19 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
+import Footer from '../Footer/Footer';
 import './Home.css'
 const Home = () => {
     return (
         <div>
-            <div className="banner">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-xl-6 col-md-8 col-12">
-                            <div class="hero-content">
-                                <h1>Providing Quality Health Care. Your Health is Our Top Priority with <span>Comprehensive</span></h1>
-                                <p>Our skilled doctors have tremendous experience with wide
-                                    range of diseases to serve the needs of our patients.</p>
-                                <button className="btn">Appointment Now</button>
-                            </div>
-                        </div>
+            {/* Banner  */}
+            <Banner></Banner>
 
-                    </div>
-                </div>
-            </div>
+
+            {/* Footer  */}
+
+            <Footer></Footer>
+
+
         </div>
     );
 };
